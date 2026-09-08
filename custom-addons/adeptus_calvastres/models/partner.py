252697bccb1605c6c9ef2faec5158e9f9f7a9d15
Alazne <alazne@turingtech.es>
2026-09-08 17:31:30 +0200

    fix: remove help text from member_date field in Partner model

--- custom-addons/adeptus_calvastres/models/partner.py
+++ custom-addons/adeptus_calvastres/models/partner.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    member_date = fields.Date(string='Member Since', help='Date when the partner became a member of the association')
+    member_date = fields.Date(string='Member Since')
