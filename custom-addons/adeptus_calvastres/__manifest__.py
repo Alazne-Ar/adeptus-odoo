84a3f1a83e52c5205c3dbc685ede991b3fb4d1bd
Alazne <alazne@turingtech.es>
2026-09-08 17:30:54 +0200

    feat: add missing view files to __manifest__.py for Adeptus Calvastres module

--- custom-addons/adeptus_calvastres/__manifest__.py
+++ custom-addons/adeptus_calvastres/__manifest__.py
@@ -8,6 +8,9 @@
     ],
     'data': [
     'security/ir.model.access.csv',
+    'views/booking_views.xml',
+    'views/partner_views.xml',
+    'views/menus.xml',
     ],
     'installable': True,
     'application': True,
