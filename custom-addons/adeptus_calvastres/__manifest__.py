2004647c5c94e918cfc14ba9020613b1457ea7a7
Alazne <alazne@turingtech.es>
2026-09-08 15:48:07 +0200

    feat: add __manifest__.py file for Adeptus Calvastres module

---
+++ custom-addons/adeptus_calvastres/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Adeptus Calvastres',
+    'version': '1.0',
+    'category': 'Association',
+    'summary': 'Association member, game and finance management',
+    'depends': [
+        'base'
+    ],
+    'data': [
+    'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
