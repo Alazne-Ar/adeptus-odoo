5994a2e77558dbacf3e6fbfef562d81972ea690a
Alazne <alazne@turingtech.es>
2026-09-09 17:30:30 +0200

    fix: add missing newline at end of booking.py file

--- custom-addons/adeptus_calvastres/models/booking.py
+++ custom-addons/adeptus_calvastres/models/booking.py
@@ -5,4 +5,5 @@ class Booking(models.Model):
     _description = 'Booking tables'
 
     name = fields.Char(string='Name', required=True)
+    hour = fields.Float(string='Hour', required=True, help='The hour of the booking')
     table_id = fields.Many2one('adeptus_calvastres.table', string='Table', required=True, help='The table that is booked')
