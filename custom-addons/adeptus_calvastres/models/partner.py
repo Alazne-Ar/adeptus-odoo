6541884d32ab9fd469c4a3d0caa2a349062dab76
Alazne <alazne@turingtech.es>
2026-09-10 14:39:58 +0200

    feat: define Partner model with fields for name, surname, email, mobile, member status, and bookings

--- custom-addons/adeptus_calvastres/models/partner.py
+++ custom-addons/adeptus_calvastres/models/partner.py
@@ -1,7 +1,15 @@
 from odoo import models, fields
 
 class Partner(models.Model):
-    _inherit = 'res.partner'
+    _name = 'adeptus_calvastres.partner'
+    _description = 'Partner'
 
+    name = fields.Char(string='Name', required=True)
+    surname = fields.Char(string='Surname')
+    email = fields.Char(string='Email', required=True)
+    mobile = fields.Char(string='Mobile', required=True)
     member_date = fields.Date(string='Member Since')
     is_member = fields.Boolean(string="Is Member", default=False)
+    bookings = fields.One2many('adeptus_calvastres.booking', 'partner_id', 
+                               string="Bookings", 
+                               help="The bookings made by the partner")
