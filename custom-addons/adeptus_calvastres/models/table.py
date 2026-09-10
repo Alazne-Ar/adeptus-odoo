3b91c4ff32a02781060a2052dd03e305c2308058
Alazne <alazne@turingtech.es>
2026-09-10 14:39:28 +0200

    feat: add bookings field to Table model for managing table bookings

--- custom-addons/adeptus_calvastres/models/table.py
+++ custom-addons/adeptus_calvastres/models/table.py
@@ -6,3 +6,4 @@ class Table(models.Model):
 
     name = fields.Char(string='Name', required=True)
     empty_table = fields.Boolean(string='Empty Table', default=True, help='Indicates if the table is empty or not')
+    bookings = fields.One2many('adeptus_calvastres.booking', 'table_id', string="Bookings", help="The bookings for this table")
