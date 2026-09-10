f5ba1fc8907c925d66910ac4d5cd94b7858736d4
Alazne <alazne@turingtech.es>
2026-09-10 17:14:58 +0200

    feat: add commented position fields for table model

--- custom-addons/adeptus_calvastres/models/table.py
+++ custom-addons/adeptus_calvastres/models/table.py
@@ -7,3 +7,6 @@ class Table(models.Model):
     name = fields.Char(string='Name', required=True)
     empty_table = fields.Boolean(string='Empty Table', default=True, help='Indicates if the table is empty or not')
     bookings = fields.One2many('adeptus_calvastres.booking', 'table_id', string="Bookings", help="The bookings for this table")
+
+    # pos_X = fields.Integer(string='Position X (pixels)', required=True, help='The X position of the table')
+    # pos_Y = fields.Integer(string='Position Y (pixels)', required=True, help='The Y position of the table')
