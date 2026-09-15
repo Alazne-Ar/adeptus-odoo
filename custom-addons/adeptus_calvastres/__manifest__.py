2b4368c308a47c5324140c8089e32b93134a9081
Alazne <alazne@turingtech.es>
2026-09-15 14:09:35 +0200

    refactor: standardize quotes in __manifest__.py for consistency

--- custom-addons/adeptus_calvastres/__manifest__.py
+++ custom-addons/adeptus_calvastres/__manifest__.py
@@ -1,18 +1,17 @@
 {
-    'name': 'Adeptus Calvastres',
-    'version': '1.0',
-    'category': 'Association',
-    'summary': 'Association member, game and finance management',
-    'depends': ['base'],
-    'application': True,
-
-    'data': [
-    'security/ir.model.access.csv',
-    'views/adeptus_partner_views.xml',
-    'views/adeptus_table_views.xml',
-    'views/adeptus_booking_views.xml',
-    'views/adeptus_menu.xml',
+    "name": "Adeptus Calvastres",
+    "version": "1.0",
+    "category": "Association",
+    "summary": "Association member, game and finance management",
+    "depends": ["base"],
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/adeptus_partner_views.xml",
+        "views/adeptus_table_views.xml",
+        "views/adeptus_booking_views.xml",
+        "views/adeptus_menu.xml",
+        "data/product_data.xml",
     ],
-
-    'installable': True,
+    "installable": True,
 }
